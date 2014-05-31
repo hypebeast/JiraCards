63d554ec35fdfd547733b3b4555696799d4f311d
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-05-31 15:20:58 +0200

    Bumped version number

--- jiracards/__init__.py
+++ jiracards/__init__.py
@@ -1,7 +1,7 @@
 #! ../env/bin/python
 
 # The package version
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 # Defines the list of module names to be imported, when "from package import *" is encountered
 __all__ = []
