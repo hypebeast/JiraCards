aa373f847fd63580934d349206201ced16c25f65
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-06-14 22:41:01 +0200

    Add support for printing individual issues

--- jiracards/jiracards.py
+++ jiracards/jiracards.py
@@ -37,7 +37,34 @@ def getBoardIssues(jira_url, boardId, user="", password=""):
     else:
         r = requests.get(jira_url + BOARD_API + boardId, auth=HTTPBasicAuth(user, password))
     data = r.json()
-    return data['issuesData']['issues']
+
+    issues = []
+    for rawIssue in data['issuesData']['issues']:
+        issue = Issue()
+        issue.key = rawIssue['key']
+        issue.summary = rawIssue['summary']
+        issue.typeName = rawIssue['typeName']
+        issues.append(issue)
+
+    return issues
+
+def getIssues(jira_url, issues, user="", password=""):
+    result = []
+    for issue in issues:
+        result.append(getIssue(jira_url, issue, user, password))
+    return result
+
+def getIssue(jira_url, issue, user="", password=""):
+    if user == "":
+        r = requests.get(jira_url + ISSUE_API + issue)
+    else:
+        r = requests.get(jira_url + ISSUE_API + issue, auth=HTTPBasicAuth(user, password))
+    data = r.json()
+    issue = Issue()
+    issue.key = data['key']
+    issue.summary = data['fields']['summary']
+    issue.typeName = data['fields']['issuetype']['name']
+    return issue
 
 def compileTemplate(tickets, template=""):
     # Check if a template was specified
@@ -52,12 +79,12 @@ def compileTemplate(tickets, template=""):
 
 def setIssuesColor(tickets, colors, defaultColor):
     for ticket in tickets:
-        if 'typeName' in ticket:
-            issueType = ticket['typeName'].lower().strip().replace(' ', '_').replace('-', '_')
+        if ticket.typeName != '':
+            issueType = ticket.typeName.lower().strip().replace(' ', '_').replace('-', '_')
             if issueType in colors:
-                ticket['cardcolor'] = colors[issueType]
+                ticket.cardcolor = colors[issueType]
             else:
-                ticket['cardcolor'] = defaultColor
+                ticket.cardcolor = defaultColor
 
 def getColors(config, section, issueTypes, colors):
     for issueType in issueTypes:
@@ -72,6 +99,15 @@ def has_key(config, section, key):
         return True
 
     return False
+
+
+class Issue:
+    def __init__(self):
+        self.key = ""
+        self.summary = ""
+        self.typeName = ""
+        self.cardcolor = ""
+
 
 # define an application base controller
 class AppBaseController(controller.CementBaseController):
@@ -103,98 +139,122 @@ class AppBaseController(controller.CementBaseController):
             (['-b', '--board'], dict(action='store', help='Jira board ID to get the tickets', dest='board')),
             (['-t', '--template'], dict(action='store', help='Template filename. The template file must be located in the template folder.', dest='template')),
             (['-c', '--config'], dict(action='store', help='Config file', dest='config')),
-            (['--color'], dict(action='store', help='Cards color (overrides the color definition from the config file)', dest='color'))
+            (['--color'], dict(action='store', help='Cards color (overrides the color definition from the config file)', dest='color')),
+            (['issues'], dict(nargs='*', help='Issue keys'))
             ]
 
-    @controller.expose(hide=False, aliases=['gen'], help='Read issues from a Jira Board and creates a card for every found issue')
-    def default(self):
-        jira = self.app.config.get('controller.base', 'jira')
-        user = self.app.config.get('controller.base', 'user')
-        password = self.app.config.get('controller.base', 'password')
-        outputFile = self.app.config.get('controller.base', 'output')
-        board = self.app.config.get('controller.base', 'board')
-        config = self.app.config.get('controller.base', 'config')
-        issueTypes = self.app.config.get('controller.base', 'issueTypes')
-        defaultIssueColor = self.app.config.get('controller.base', 'default_issue_color')
-        template = None
+    def readConfig(self):
+        # Set options from the default config
+        self.jira = self.app.config.get('controller.base', 'jira')
+        self.user = self.app.config.get('controller.base', 'user')
+        self.password = self.app.config.get('controller.base', 'password')
+        self.outputFile = self.app.config.get('controller.base', 'output')
+        self.board = self.app.config.get('controller.base', 'board')
+        self.config = self.app.config.get('controller.base', 'config')
+        self.issueTypes = self.app.config.get('controller.base', 'issueTypes')
+        self.defaultIssueColor = self.app.config.get('controller.base', 'default_issue_color')
+        self.colors = {}
+        self.template = None
 
         # Set options from the config file
         if has_key(self.app.config, 'jira-cards', 'jira'):
-            jira = self.app.config.get('jira-cards', 'jira')
+            self.jira = self.app.config.get('jira-cards', 'jira')
         if has_key(self.app.config, 'jira-cards', 'user'):
-            user = self.app.config.get('jira-cards', 'user')
+            self.user = self.app.config.get('jira-cards', 'user')
         if has_key(self.app.config, 'jira-cards', 'password'):
-            password = self.app.config.get('jira-cards', 'password')
+            self.password = self.app.config.get('jira-cards', 'password')
         if has_key(self.app.config, 'jira-cards', 'output'):
-            outputFile = self.app.config.get('jira-cards', 'output')
+            self.outputFile = self.app.config.get('jira-cards', 'output')
         if has_key(self.app.config, 'jira-cards', 'board'):
-            board = self.app.config.get('jira-cards', 'board')
+            self.board = self.app.config.get('jira-cards', 'board')
         if has_key(self.app.config, 'jira-cards', 'template'):
-            template = self.app.config.get('jira-cards', 'template')
+            self.template = self.app.config.get('jira-cards', 'template')
         if has_key(self.app.config, 'jira-cards', 'issueTypes'):
-            issueTypes = [x.strip() for x in self.app.config.get('jira-cards', 'issueTypes').split(',')]
+            self.issueTypes = [x.strip() for x in self.app.config.get('jira-cards', 'issueTypes').split(',')]
         if has_key(self.app.config, 'jira-cards', 'issueTypes'):
-            defaultIssueColor = self.app.config.get('jira-cards', 'default_issue_color')
+            self.defaultIssueColor = self.app.config.get('jira-cards', 'default_issue_color')
 
         # Set options from command line arguments
         if self.app.pargs.jira:
-            jira = self.app.pargs.jira
+            self.jira = self.app.pargs.jira
         if self.app.pargs.user:
-            user = self.app.pargs.user
+            self.user = self.app.pargs.user
         if self.app.pargs.password:
-            password = self.app.pargs.password
+            self.password = self.app.pargs.password
         if self.app.pargs.output:
-            outputFile = self.app.pargs.output
+            self.outputFile = self.app.pargs.output
         if self.app.pargs.board:
-            board = self.app.pargs.board
+            self.board = self.app.pargs.board
         if self.app.pargs.template:
-            template = self.app.pargs.template
+            self.template = self.app.pargs.template
         if self.app.pargs.config:
-            config = self.app.pargs.config
+            self.config = self.app.pargs.config
 
         # Get the card colors from the config and set the color to the appropriate issue type
-        colors = {}
-        getColors(self.app.config, 'controller.base', issueTypes, colors)
-        getColors(self.app.config, 'jira-cards', issueTypes, colors)
+        getColors(self.app.config, 'controller.base', self.issueTypes, self.colors)
+        getColors(self.app.config, 'jira-cards', self.issueTypes, self.colors)
 
-        # If a color on the command line was specified override the colors from
-        # the config file
-        if self.app.pargs.color:
-            for color in colors:
-                color = self.app.pargs.color
-
-        if not jira:
-            self.app.log.info('No JIRA URL specified. Please, provide a Jira URL.')
-            sys.exit(1)
-
-        self.app.log.info('Retrieving Jira issues')
-        issues = getBoardIssues(jira, board, user, password)
-
-        # Set color for every issue
-        setIssuesColor(issues, colors, defaultIssueColor)
-
-        self.app.log.info('Generating cards')
-        if template:
-            output = compileTemplate(issues, template)
+    def generateCards(self, issues):
+        if self.template:
+            output = compileTemplate(issues, self.template)
         else:
             output = compileTemplate(issues)
 
-        with codecs.open(outputFile, 'w', 'utf-8-sig') as file:
+        with codecs.open(self.outputFile, 'w', 'utf-8-sig') as file:
             file.write(output)
 
+    @controller.expose(hide=False, aliases=['generate'], help='Read issues from a Jira Board and creates a card for every found issue')
+    def default(self):
+        self.readConfig()
+
+        if not self.jira:
+            self.app.log.info('No JIRA URL specified. Please, provide a Jira URL.')
+            sys.exit(1)
+
+        self.app.log.info('Getting Jira issues')
+        issues = getBoardIssues(self.jira, self.board, self.user, self.password)
+
+        self.app.log.info("Found " + str(len(issues)) + " issues")
+
+        # Set color for every issue
+        setIssuesColor(issues, self.colors, self.defaultIssueColor)
+
+        if self.app.pargs.color:
+            for issue in issues:
+                issue.cardcolor = self.app.pargs.color
+
+        self.app.log.info('Generating cards')
+        self.generateCards(issues)
         self.app.log.info('Done! All cards generated.')
 
     @controller.expose(help='Create cards for the specified Jira issues')
-    def tickets(self):
-        pass
+    def issues(self):
+        self.readConfig()
+
+        if not self.jira:
+            self.app.log.info('No JIRA URL specified. Please, provide a Jira URL.')
+            sys.exit(1)
+
+        self.app.log.info('Getting Jira issues')
+        issues = getIssues(self.jira, self.app.pargs.issues, self.user, self.password)
+
+        setIssuesColor(issues, self.colors, self.defaultIssueColor)
+
+        if self.app.pargs.color:
+            for issue in issues:
+                issue.cardcolor = self.app.pargs.color
+
+        self.app.log.info('Generating cards')
+        self.generateCards(issues)
+        self.app.log.info('Done! All cards generated.')
 
     @controller.expose(help='Prints the default template to stdout')
     def printtemplate(self):
         print DEFAULT_TEMPLATE
 
-    @controller.expose(help='Prints the available template data to stdout')
-    def printdata(self):
-        pass
+    # @controller.expose(help='Prints the available template data to stdout')
+    # def printdata(self):
+    #     pass
 
 
 class MyApp(foundation.CementApp):
